b5cf946e3f0ac389836afa209053166cbeb9f66d
Ts3nguun <0805tsenguun@gmail.com>
2024-07-30 13:35:48 +0800

    tiny up

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -15,16 +15,25 @@ export default function Home() {
       });
   }, []);
   return (
-    <main>
-      <div>
+    <div className="container mx-auto ">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mx-auto">
         {articles.map((item) => (
-          <div key={item.id}>
-            <Link href={item.url} target="_blank">
-              {item.title}
-            </Link>
+          <div key={item.id} className="shadow-lg card bg-base-100 mx-auto  ">
+            <div className="card-body">
+              <div className="badge badge-primary">primary</div>
+              <img src={item.social_image} width={500} height={500} />
+              <Link href={item.url} target="_blank">
+                {item.title}
+              </Link>
+              <div className="flex gap-4">
+                <img src={item.user.profile_image_90} width={50} height={50} />
+                <div>{item.user.name}</div>
+                <div>{item.published_at}</div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
-    </main>
+    </div >
   );
 }
